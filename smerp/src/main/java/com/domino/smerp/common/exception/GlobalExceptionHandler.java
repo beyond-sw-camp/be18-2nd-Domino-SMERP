01bf99806a8f41d0c9366bed089180530fc96255
IIIjong <101960311+IIIjong@users.noreply.github.com>
2025-09-04 15:42:06 +0900

    [feat] 공통 예외 처리 (#22)
    * [feat] 공통 예외 처리 관련 기능 구현
    
    * [feat] 거래처 관련 에러코드 추가
    
    * [feat] 예외 처리 콘솔 추가, http 상태 번호 추가

---
+++ smerp/src/main/java/com/domino/smerp/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.domino.smerp.common.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException ex) {
+        ErrorCode errorCode = ex.getErrorCode();
+        log.info("[{}] {}", errorCode.getCode(), errorCode.getMessage());
+        return new ResponseEntity<>(
+            new ErrorResponse(errorCode.getCode(), errorCode.getMessage(),errorCode.getStatus().value()), errorCode.getStatus()
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        return ResponseEntity.internalServerError()
+                             .body(new ErrorResponse("INTERNAL_SERVER_ERROR", ex.getMessage(),
+                                 HttpStatus.INTERNAL_SERVER_ERROR.value()));
+    }
+}
