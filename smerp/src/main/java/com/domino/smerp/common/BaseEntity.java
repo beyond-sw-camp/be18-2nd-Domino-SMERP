ea1d3621809b494dae626ec6d553ee5515c8af3e
최정필 <103472023+wjdvlf5456@users.noreply.github.com>
2025-09-08 11:07:31 +0900

    [feat] BaseEntity 구현 (#31)

--- smerp/src/main/java/com/domino/smerp/common/BaseEntity.java
+++ smerp/src/main/java/com/domino/smerp/common/BaseEntity.java
@@ -3,22 +3,32 @@ package com.domino.smerp.common;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import java.time.Instant;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @MappedSuperclass
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
-public abstract class TimestampedEntity {
+public abstract class BaseEntity {
 
   @CreatedDate
   @Column(name = "created_at", nullable = false, updatable = false)
-  private LocalDateTime createdAt;   // INSERT 시 자동 세팅
+  private Instant createdAt;
 
   @LastModifiedDate
   @Column(name = "updated_at")
-  private LocalDateTime updatedAt;   // UPDATE 시 자동 세팅
+  private Instant updatedAt;
+
 }
+
--- smerp/src/main/java/com/domino/smerp/item/ItemController.java
+++ smerp/src/main/java/com/domino/smerp/item/ItemController.java
@@ -1,12 +1,12 @@
 package com.domino.smerp.item;
 
-import com.domino.smerp.item.dto.request.ItemRequest;
+import com.domino.smerp.item.dto.request.CreateItemRequest;
+import com.domino.smerp.item.dto.request.UpdateItemRequest;
 import com.domino.smerp.item.dto.request.UpdateItemStatusRequest;
 import com.domino.smerp.item.dto.response.ItemResponse;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,7 +25,8 @@ public class ItemController {
   private final ItemService itemService;
 
   @PostMapping
-  public ResponseEntity<ItemResponse> createItem(@RequestBody ItemRequest request) {
+  public ResponseEntity<ItemResponse> createItem(final @Valid @RequestBody CreateItemRequest request) {
+
     return ResponseEntity.ok(itemService.createItem(request));
   }
 
@@ -36,24 +36,24 @@ public class ItemController {
   }
 
   @GetMapping("/{item-id}")
-  public ResponseEntity<ItemResponse> getItemById(@PathVariable("item-id") Long itemId) {
+  public ResponseEntity<ItemResponse> getItemById(@PathVariable("item-id") final Long itemId) {
     return ResponseEntity.ok(itemService.getItemById(itemId));
   }
 
   @PatchMapping("/{item-id}")
-  public ResponseEntity<ItemResponse> updateItem(@PathVariable("item-id") Long itemId,
-      @RequestBody ItemRequest request) {
+  public ResponseEntity<ItemResponse> updateItem(@PathVariable("item-id") final Long itemId,
+      final @Valid @RequestBody UpdateItemRequest request) {
     return ResponseEntity.ok(itemService.updateItem(itemId, request));
   }
 
   @PatchMapping("/{item-id}/status")
-  public ResponseEntity<ItemResponse> updateItemStatus(@PathVariable("item-id") Long itemId,
-      @RequestBody UpdateItemStatusRequest request) {
+  public ResponseEntity<ItemResponse> updateItemStatus(@PathVariable("item-id") final Long itemId,
+      final @Valid @RequestBody UpdateItemStatusRequest request) {
     return ResponseEntity.ok(itemService.updateItemStatus(itemId, request));
   }
 
   @DeleteMapping("/{item-id}")
-  public ResponseEntity<Void> deleteItem(@PathVariable("item-id") Long itemId) {
+  public ResponseEntity<Void> deleteItem(@PathVariable("item-id") final Long itemId) {
     itemService.deleteItem(itemId);
     return ResponseEntity.noContent().build();
   }
