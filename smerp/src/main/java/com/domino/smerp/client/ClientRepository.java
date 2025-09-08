a4e3fa593b8b07fd8615d2b0ef88c31246e6e9db
IIIjong <101960311+IIIjong@users.noreply.github.com>
2025-09-08 21:50:59 +0900

    [feat] 회사명으로 찾는 메소드 추가 (#37)

--- smerp/src/main/java/com/domino/smerp/client/ClientRepository.java
+++ smerp/src/main/java/com/domino/smerp/client/ClientRepository.java
@@ -1,8 +1,10 @@
 package com.domino.smerp.client;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    Optional<Client> findByCompanyName(String companyName);
 }
