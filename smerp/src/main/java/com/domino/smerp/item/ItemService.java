4bbb4bbe2695e402b00aeff43aa072e81d205ae6
최정필 <103472023+wjdvlf5456@users.noreply.github.com>
2025-09-10 20:18:14 +0900

    [refactor] 품목 dto분리, findItemById로 통합, querydsl 추가 (#40)
    * [refactor] mapToResponseDto메소드 -> ItemResponse.java로
    
    * [refactor] item 외래키 @ForeignKey(ConstraintMode.NO_CONSTRAINT 로 변경
    
    * [refactor] item ErrorCode 미반영
    
    * [refactor] item테이블 소프트딜리트 적용
    
    * [refactor] item테이블 dto @Valid 추가
    
    * [refactor] ResponseDto 분리
    
    * ItemRepository JpaSpecificationExecutor<Item> 추가
    
    * [refactor] BaseEntity updated_at null허용 및 품목 구분 별 조회 구현
    
    * [refactor] querydsl 적용
    
    * [refactor] 매개변수 이름 통일

--- smerp/src/main/java/com/domino/smerp/item/ItemService.java
+++ smerp/src/main/java/com/domino/smerp/item/ItemService.java
@@ -1,27 +1,33 @@
 package com.domino.smerp.item;
 
+import com.domino.smerp.common.dto.PageResponse;
 import com.domino.smerp.item.dto.request.CreateItemRequest;
+import com.domino.smerp.item.dto.request.ItemSearchRequest;
 import com.domino.smerp.item.dto.request.UpdateItemRequest;
 import com.domino.smerp.item.dto.request.UpdateItemStatusRequest;
-import com.domino.smerp.item.dto.response.ItemResponse;
+import com.domino.smerp.item.dto.response.ItemDetailResponse;
+import com.domino.smerp.item.dto.response.ItemListResponse;
+import com.domino.smerp.item.dto.response.ItemStatusResponse;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ItemService {
 
   // 품목 생성
-  ItemResponse createItem(final CreateItemRequest request);
+  ItemDetailResponse createItem(final CreateItemRequest request);
 
   // 품목 목록 조회
-  List<ItemResponse> getItems();
+  PageResponse<ItemListResponse> searchItems(final ItemSearchRequest keyword, final Pageable pageable);
 
   // 품목 상세 조회
-  ItemResponse getItemById(final Long itemId);
+  ItemDetailResponse getItemById(final Long itemId);
 
   // 품목 수정(품목 구분 포함)
-  ItemResponse updateItem(final Long itemId, final UpdateItemRequest request);
+  ItemDetailResponse updateItem(final Long itemId, final UpdateItemRequest request);
 
   // 품목 안전재고 / 사용여부 수정
-  ItemResponse updateItemStatus(final Long itemId, final UpdateItemStatusRequest request);
+  ItemStatusResponse updateItemStatus(final Long itemId, final UpdateItemStatusRequest request);
 
   // 품목 삭제
   void deleteItem(final Long itemId);
