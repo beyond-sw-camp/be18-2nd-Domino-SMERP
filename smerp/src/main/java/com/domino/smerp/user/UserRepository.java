9f89ae887bde1ae8a950f4e520380130d59a6bf8
IIIjong <pjwfish@naver.com>
2025-09-11 21:31:03 +0900

    [feat] 이슈번호#44 유저 검색 구현

--- smerp/src/main/java/com/domino/smerp/user/UserRepository.java
+++ smerp/src/main/java/com/domino/smerp/user/UserRepository.java
@@ -3,11 +3,12 @@ package com.domino.smerp.user;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,Long>, QuerydslPredicateExecutor<User> {
     boolean existsByLoginId(String loginId);
     boolean existsByEmail(String email);
     boolean existsByPhone(String phone);
