42e6bdb93520bcf58abb6c420533f55d7de272fb
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-09-12 14:41:30 +0900

    Feature/19 warehouse crud (#30)
    * [feat] warehouse crud
    
    * [feat] column name 추가
    
    * [feat] constants folder
    
    * [feat] location 주석처리
    
    * [feat] dto final
    
    * [feat] Warehouse annotation 추가
    
    * [feat] 빈 창고 조회
    
    * [feat] service method parameters final 추가
    
    * [feat] @Transactional(readOnly = true)
    
    * [feat] repository method 추가
    
    * [feat] location 주석처리
    
    * [feat] extend base entity
    
    * [feat] extend base entity
    
    * [fix] dto 불필요 값 제거, null 값 오류 해결

---
+++ smerp/src/main/java/com/domino/smerp/warehouse/service/WarehouseService.java
@@ -0,0 +1,24 @@
+package com.domino.smerp.warehouse.service;
+
+import com.domino.smerp.warehouse.Warehouse;
+import com.domino.smerp.warehouse.dto.WarehouseRequest;
+import com.domino.smerp.warehouse.dto.response.WarehouseIdListResponse;
+import com.domino.smerp.warehouse.dto.response.WarehouseResponse;
+import java.util.List;
+
+public interface WarehouseService {
+
+  WarehouseResponse getWarehouseById(final Long id);
+
+  List<WarehouseResponse> getAllWarehouses();
+
+  void deleteWarehouse(final Long id);
+
+  WarehouseResponse updateWarehouse(final Long id, final WarehouseRequest warehouseRequest);
+
+  WarehouseResponse createWarehouse(final WarehouseRequest warehouseRequest);
+
+  //WarehouseIdListResponse getAllUnFilledWarehouses();
+
+  WarehouseResponse toWarehouseResponse(final Warehouse warehouse);
+}
