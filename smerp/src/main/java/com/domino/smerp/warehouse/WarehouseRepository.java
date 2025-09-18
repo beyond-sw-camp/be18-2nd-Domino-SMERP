9db7b97d26ec201ab98f68564838d7cd01001ff4
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-09-18 20:04:27 +0900

    [feat] 빈 위치 warehouse 찾기

--- smerp/src/main/java/com/domino/smerp/warehouse/WarehouseRepository.java
+++ smerp/src/main/java/com/domino/smerp/warehouse/WarehouseRepository.java
@@ -1,7 +1,9 @@
 package com.domino.smerp.warehouse;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,13 +12,13 @@ public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
   Boolean existsByName(String name);
 
   Optional<Warehouse> findByItemId(Long itemId);
-/*
+
   @Query("SELECT DISTINCT w.id FROM Warehouse w " +
       "WHERE NOT EXISTS (" +
       "  SELECT l FROM Location l " +
       "  WHERE l.warehouse = w AND l.filled = true" +
       ")")
   List<Warehouse> findWarehousesWithFilledFalseLocations();
-*/
+
 }
 
