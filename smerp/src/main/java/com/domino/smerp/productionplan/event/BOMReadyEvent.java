76196449cff6d7ea6f76bd70fbfbcf5575d08893
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-09-22 00:52:59 +0900

    [feat] bom ready event

--- smerp/src/main/java/com/domino/smerp/productionplan/event/BOMReadyEvent.java
+++ smerp/src/main/java/com/domino/smerp/productionplan/event/BOMReadyEvent.java
@@ -1,6 +1,11 @@
 package com.domino.smerp.productionplan.event;
 
-//품목에 대한 bom 생성 시 반환
-public class BOMReadyEvent {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+//품목에 대한 bom 생성 시 반환
+@Getter
+@RequiredArgsConstructor
+public class BOMReadyEvent {
+  private final Long ProductionPlanId;
 }
