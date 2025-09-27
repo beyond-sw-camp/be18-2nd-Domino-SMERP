02b8a863d366bad81dc4c8770050c3e81f7a13e1
wjdvlf5456 <wjdvlf1407@gmail.com>
2025-09-27 19:42:42 +0900

    [WIP]

--- smerp/src/main/java/com/domino/smerp/bom/service/cache/BomCacheService.java
+++ smerp/src/main/java/com/domino/smerp/bom/service/cache/BomCacheService.java
@@ -9,6 +9,8 @@ public interface BomCacheService {
 
   List<BomCostCache> getCacheByRootItemId(final Long rootItemId);
 
+
+  // TODO: 전체 캐시 재생성 시점 생각하기 
   // BOM 전체 캐시 재생성
   void rebuildAllBomCache();
 
