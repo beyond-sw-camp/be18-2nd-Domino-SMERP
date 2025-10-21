90cb4cb03b547fe49d8147c3bc28c5443fbf56d1
wjdvl <wjdvl@25.20.245.87>
2025-10-22 00:53:52 +0900

    [feat] 최종

--- smerp/src/main/java/com/domino/smerp/item/Item.java
+++ smerp/src/main/java/com/domino/smerp/item/Item.java
@@ -15,8 +15,6 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
