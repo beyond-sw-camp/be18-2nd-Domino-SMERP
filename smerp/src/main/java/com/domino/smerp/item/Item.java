277ff19bba34107051439192f41e44711f3d4023
wjdvl <wjdvl@25.20.245.87>
2025-10-22 00:23:42 +0900

    pre-commit spotless auto test

--- smerp/src/main/java/com/domino/smerp/item/Item.java
+++ smerp/src/main/java/com/domino/smerp/item/Item.java
@@ -19,6 +19,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
