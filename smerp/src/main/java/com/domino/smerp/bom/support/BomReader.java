d6aaaddda73bbc80da0dceb2d9d15d9ce4f79dfc
wjdvlf5456 <wjdvlf1407@gmail.com>
2025-12-24 17:05:45 +0900

    [refactor] BomReader로 조회 메서드 책임 분리

--- smerp/src/main/java/com/domino/smerp/bom/support/BomReader.java
+++ smerp/src/main/java/com/domino/smerp/bom/support/BomReader.java
@@ -4,6 +4,7 @@ import com.domino.smerp.bom.entity.Bom;
 import com.domino.smerp.bom.repository.BomRepository;
 import com.domino.smerp.common.exception.CustomException;
 import com.domino.smerp.common.exception.ErrorCode;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +17,10 @@ public class BomReader {
   public Bom findBomById(final Long bomId) {
     return bomRepository.findById(bomId)
         .orElseThrow(() -> new CustomException(ErrorCode.BOM_NOT_FOUND));
+  }
+
+  public List<Bom> findChildren(final Long parentItemId) {
+    return bomRepository.findByParentItem_ItemId(parentItemId);
   }
 
 }
